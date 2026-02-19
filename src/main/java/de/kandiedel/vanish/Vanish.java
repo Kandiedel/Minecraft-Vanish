e1e3596efc0d328e9ea3d48aca370448e9134f0a
Kandiedel <triebelfinn@gmail.com>
2026-02-19 22:53:26 +0100

    noTicket: Created enable and disable message

--- src/main/java/de/kandiedel/vanish/Vanish.java
+++ src/main/java/de/kandiedel/vanish/Vanish.java
@@ -1,16 +1,41 @@
 package de.kandiedel.vanish;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Vanish extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + "------------------------------------------");
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + " [ ✔ ] "
+                + ChatColor.DARK_PURPLE + getDescription().getName()
+                + ChatColor.GRAY + " successfully enabled!"
+                + ChatColor.GREEN + " [ ✔ ]");
+        getServer().getConsoleSender().sendMessage("         "
+                + ChatColor.GRAY + "Developed By "
+                + ChatColor.DARK_PURPLE + "Kandiedel " + ChatColor.GRAY + "⚒ ");
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + "------------------------------------------");
+        getServer().getConsoleSender().sendMessage("");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + "-------------------------------------------");
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + " [ ✖ ] "
+                + ChatColor.DARK_PURPLE + getDescription().getName()
+                + ChatColor.GRAY + " successfully disabled!"
+                + ChatColor.RED + " [ ✖ ]");
+        getServer().getConsoleSender().sendMessage("         "
+                + ChatColor.GRAY + "Developed By "
+                + ChatColor.DARK_PURPLE + "Kandiedel " + ChatColor.GRAY + "⚒ ");
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + "-------------------------------------------");
+        getServer().getConsoleSender().sendMessage("");
     }
 }
